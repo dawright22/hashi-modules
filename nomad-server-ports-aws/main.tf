957a8029598d74ad251732d71f7d04358f6d4682
dawright22 <53329137+dawright22@users.noreply.github.com>
2019-11-06 17:25:24 +1100

    v12

--- nomad-server-ports-aws/main.tf
+++ nomad-server-ports-aws/main.tf
@@ -6,37 +6,38 @@ terraform {
 module "nomad_client_ports_aws" {
   source = "github.com/hashicorp-modules/nomad-client-ports-aws"
 
-  create      = "${var.create}"
-  name        = "${var.name}"
-  vpc_id      = "${var.vpc_id}"
-  cidr_blocks = "${var.cidr_blocks}"
-  tags        = "${var.tags}"
+  create      = var.create
+  name        = var.name
+  vpc_id      = var.vpc_id
+  cidr_blocks = var.cidr_blocks
+  tags        = var.tags
 }
 
 # The port used for the gossip protocol for cluster membership. Both TCP and
 # UDP should be routable between the server nodes on this port
 # https://www.nomadproject.io/docs/agent/configuration/index.html#serf-2
 resource "aws_security_group_rule" "serf_tcp" {
-  instance_count = "${var.create ? 1 : 0}"
+  count = var.create ? 1 : 0
 
-  security_group_id = "${module.nomad_client_ports_aws.nomad_client_sg_id}"
+  security_group_id = module.nomad_client_ports_aws.nomad_client_sg_id
   type              = "ingress"
   protocol          = "tcp"
   from_port         = 4648
   to_port           = 4648
-  cidr_blocks       = ["${var.cidr_blocks}"]
+  cidr_blocks       = var.cidr_blocks
 }
 
 # The port used for the gossip protocol for cluster membership. Both TCP and
 # UDP should be routable between the server nodes on this port
 # https://www.nomadproject.io/docs/agent/configuration/index.html#serf-2
 resource "aws_security_group_rule" "serf_udp" {
-  instance_count = "${var.create ? 1 : 0}"
+  count = var.create ? 1 : 0
 
-  security_group_id = "${module.nomad_client_ports_aws.nomad_client_sg_id}"
+  security_group_id = module.nomad_client_ports_aws.nomad_client_sg_id
   type              = "ingress"
   protocol          = "udp"
   from_port         = 4648
   to_port           = 4648
-  cidr_blocks       = ["${var.cidr_blocks}"]
+  cidr_blocks       = var.cidr_blocks
 }
+
--- nomad-server-ports-aws/output.tf
+++ nomad-server-ports-aws/output.tf
@@ -1,3 +1,4 @@
 output "nomad_server_sg_id" {
-  value = "${module.nomad_client_ports_aws.nomad_client_sg_id}"
+  value = module.nomad_client_ports_aws.nomad_client_sg_id
 }
+
