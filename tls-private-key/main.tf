381d80dacfbc0bf61cda761dcd2ae5b6267099c1
dawright22 <53329137+dawright22@users.noreply.github.com>
2019-11-14 10:18:30 +1100

    Update main.tf

--- tls-private-key/main.tf
+++ tls-private-key/main.tf
@@ -1,16 +1,16 @@
 terraform {
-  required_version = ">= 0.11.6"
+  required_version = ">= 0.12.0"
 }
 
 resource "random_id" "name" {
-  count = "${var.create ? 1 : 0}"
+  count = var.create ? 1 : 0
 
   byte_length = 4
   prefix      = "${var.name}-"
 }
 
 resource "tls_private_key" "key" {
-  count = "${var.create ? 1 : 0}"
+  count = var.create ? 1 : 0
 
   algorithm   = "${var.algorithm}"
   rsa_bits    = "${var.rsa_bits}"
@@ -18,7 +18,7 @@ resource "tls_private_key" "key" {
 }
 
 resource "null_resource" "download_private_key" {
-  count = "${var.create ? 1 : 0}"
+  count = var.create ? 1 : 0
 
   provisioner "local-exec" {
     command = "echo '${tls_private_key.key.private_key_pem}' > ${format("%s.key.pem", random_id.name.hex)} && chmod ${var.permissions} ${format("%s.key.pem", random_id.name.hex)}"
