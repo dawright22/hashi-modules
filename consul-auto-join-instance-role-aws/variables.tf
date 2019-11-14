2919bccb12a7f45b2d3d6058d3a52fd9420794d8
dawright22 <53329137+dawright22@users.noreply.github.com>
2019-11-14 11:49:33 +1100

    Delete variables.tf

--- consul-auto-join-instance-role-aws/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "create" {
-  description = "Create Module, defaults to true."
-  default     = true
-}
-
-variable "name" {
-  description = "Name for resources, defaults to \"consul-auto-join-instance-role-aws\"."
-  default     = "consul-auto-join-aws"
-}
