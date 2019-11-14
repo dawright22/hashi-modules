519eef6fa661985045fc7516b3d6363c7a7b0519
dawright22 <53329137+dawright22@users.noreply.github.com>
2019-11-14 11:50:12 +1100

    Delete main.tf

--- consul-auto-join-instance-role-aws/example/no-provision/main.tf
+++
@@ -1,6 +0,0 @@
-module "consul_auto_join_instance_role" {
-  # source = "github.com/hashicorp-modules/consul-auto-join-instance-role-aws"
-  source = "../../../consul-auto-join-instance-role-aws"
-
-  create = false
-}
