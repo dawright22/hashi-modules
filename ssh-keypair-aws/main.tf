5b2d76f1285ad91045e28ee6e9845f1b1b6f9ef8
dawright22 <53329137+dawright22@users.noreply.github.com>
2020-08-14 14:40:42 +1000

    Update main.tf

--- ssh-keypair-aws/main.tf
+++ ssh-keypair-aws/main.tf
@@ -14,5 +14,5 @@ resource "aws_key_pair" "main" {
   count = var.create ? 1 : 0
 
   key_name_prefix = "${module.tls_private_key.private_key_name}-"
-  public_key      = "${module.tls_private_key.public_key_openssh}"
+  public_key      = module.tls_private_key.public_key_openssh
 }
